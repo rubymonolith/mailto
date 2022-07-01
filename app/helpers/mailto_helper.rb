1a265fcf5fc3fa8b2f6bc61407ffc0b0ce8dc749
Brad Gessler <bradgessler@gmail.com>
2022-07-01 14:00:32 -0700

    Fix kwargs update

--- app/helpers/mailto_helper.rb
+++ app/helpers/mailto_helper.rb
@@ -2,7 +2,7 @@
 module MailtoHelper
   def mail_to_template(template, text, **kwargs)
     contents = File.read Rails.root.join("app/mailtos/#{template}.txt.erb")
-    parser = Frontmatter.new render inline: contents
+    parser = Mailto::Frontmatter.new render inline: contents
     data = parser.data.merge("body": parser.body)
     email = data.delete("to")
     mail_to email, text, **data.merge(kwargs)
